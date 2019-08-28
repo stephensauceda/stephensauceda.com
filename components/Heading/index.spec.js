e1bb27375b678c3d6c8ee4b61b4392ce81a28b5c
Stephen Sauceda <contact@stephensauceda.com>
2019-08-27 21:21:10 -0400

    convention

--- components/Heading/index.spec.js
+++ components/Heading/index.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Heading from '../Heading'
+import Heading from './'
 import { render, cleanup } from '@testing-library/react'
 
 afterEach(cleanup)
--- components/Paragraph/index.spec.js
+++ components/Paragraph/index.spec.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Paragraph from '../Paragraph'
+import Paragraph from './'
 import { render, cleanup } from '@testing-library/react'
 
 afterEach(cleanup)
