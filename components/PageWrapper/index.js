8351c67ede8454f2b66223b2db74d1579817eb31
Stephen Sauceda <contact@stephensauceda.com>
2020-04-25 20:07:33 -0400

    appease eslint

--- components/PageWrapper/index.js
+++ components/PageWrapper/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { node } from 'prop-types'
 
 const PageWrapper = ({ children }) => (
   <div className="flex flex-col justify-center items-center min-h-screen my-0 mx-auto max-w-sms-page-width w-11/12">
     {children}
   </div>
 )
+
+PageWrapper.propTypes = {
+  children: node.isRequired
+}
 
 export default PageWrapper
