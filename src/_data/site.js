939376443d24ec3ec020c14c08e312771edbfa4f
Stephen Sauceda <stephen.sauceda@gmail.com>
2022-11-27 09:05:15 -0500

    start using site data file

---
+++ src/_data/site.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Stephen Sauceda',
+  description: 'Web Developer',
+  domain: 'stephensauceda.com',
+  navigation: [
+    {
+      url: '/',
+      label: 'Home',
+    },
+    {
+      url: '/archive',
+      label: 'Archive',
+    },
+  ],
+}
