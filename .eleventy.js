5d216e1dde3c36f2f7dfbf6d12262701a3f0e6a4
Stephen Sauceda <stephen.sauceda@gmail.com>
2022-11-27 12:12:09 -0500

    add rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 const image = require('./shortcodes/image')
 const date = require('./filters/date')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/images')
   eleventyConfig.addFilter('date', date)
   eleventyConfig.addNunjucksAsyncShortcode('image', image)
+
+  eleventyConfig.addPlugin(pluginRss)
 
   return {
     dir: {
