ff321623c1bbb8df69b6a920ea65fa97d31d3d1c
Stephen Sauceda <stephen.sauceda@gmail.com>
2022-11-27 09:05:35 -0500

    move image shortcode out of config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,10 @@
-const Image = require('@11ty/eleventy-img')
+const image = require('./shortcodes/image')
 const date = require('./filters/date')
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/images')
   eleventyConfig.addFilter('date', date)
-  eleventyConfig.addNunjucksAsyncShortcode('image', async (src, alt) => {
-    const options = {
-      widths: [700],
-      urlPath: '/assets/images',
-      outputDir: './out/assets/images',
-    }
-
-    const metadata = await Image(src, options)
-    return Image.generateHTML(metadata, { alt })
-  })
+  eleventyConfig.addNunjucksAsyncShortcode('image', image)
 
   return {
     dir: {
---
+++ shortcodes/image.js
@@ -0,0 +1,14 @@
+const Image = require('@11ty/eleventy-img')
+
+async function image(src, alt) {
+  const options = {
+    widths: [700],
+    urlPath: '/assets/images',
+    outputDir: './out/assets/images',
+  }
+
+  const metadata = await Image(src, options)
+  return Image.generateHTML(metadata, { alt })
+}
+
+module.exports = image
