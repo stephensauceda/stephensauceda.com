4c92a6fb2b5ea4840758fa55c69a2fabb0d26616
Stephen Sauceda <stephen.sauceda@gmail.com>
2022-12-29 01:16:20 +0000

    a temporary fix for webmentions of redirected posts

--- filters/webmentions.js
+++ filters/webmentions.js
@@ -1,6 +1,22 @@
 module.exports = {
   getWebmentionsForUrl: (webmentions, url) => {
-    return webmentions.children.filter(entry => entry['wm-target'] === url)
+    let postUrl = url
+    const redirects = {
+      'https://stephensauceda.com/posts/2022/but-this-one-goes-to-eleventy/':
+        'https://stephensauceda.com/archive/but-this-one-goes-to-eleventy/',
+      'https://stephensauceda.com/posts/2022/building-a-new-house/':
+        'https://stephensauceda.com/archive/building-a-new-house/',
+      'https://stephensauceda.com/posts/2021/take-your-shots/':
+        'https://stephensauceda.com/archive/take-your-shots/',
+      'https://stephensauceda.com/posts/2020/i-am-everything-right-now/':
+        'https://stephensauceda.com/archive/i-am-everything-right-now/',
+    }
+
+    if (Object.keys(redirects).includes(postUrl)) {
+      postUrl = redirects[postUrl]
+    }
+
+    return webmentions.children.filter(entry => entry['wm-target'] === postUrl)
   },
   size: mentions => {
     return !mentions ? 0 : mentions.length
