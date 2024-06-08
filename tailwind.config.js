4e3ef29acab4088e1955983081eb34ad6832bf12
Stephen Sauceda <stephen.sauceda@gmail.com>
2024-06-08 14:19:42 -0400

    underline thingy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{html,njk}'],
   theme: {
+    clipPath: {
+      parallelogram: 'polygon(0 0, 90% 0, 100% 100%, 10% 100%)',
+    },
     extend: {
       screens: {
         md: '840px',
+      },
+      transitionProperty: {
+        width: 'width',
       },
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/line-clamp'),
+    require('tailwind-clip-path'),
   ],
 }
